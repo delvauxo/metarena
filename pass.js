dbdc421140c5f4d5afc1fa66151f37d957e228e6
delvauxo <delvauxo@outlook.com>
2022-01-05 23:02:41 +0100

    video bg and password input

---
+++ pass.js
@@ -0,0 +1,25 @@
+// Selectors.
+const inputPw = document.querySelector('#form-pw input')
+const video = document.querySelector('section video')
+const code = 'metaforlife'
+
+inputPw.addEventListener('keyup', function (e) {
+    // If enter is hit.
+    if (e.keyCode === 13) {
+        // Cancel the default action, if needed.
+        e.preventDefault()
+        if (inputPw.value === code) {
+            console.log(inputPw.value)
+            console.log(code)
+            console.log('Vous pouvez entrer !')
+            video.setAttribute('src', './img/home-video-pw-2.webm')
+            video.removeAttribute('repeat')
+
+        } else {
+            console.log(inputPw.value)
+            console.log(code)
+            console.log('Vous ne pouvez pas entrer !')
+        }
+        
+    }
+})
